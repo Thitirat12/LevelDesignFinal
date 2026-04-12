8721280a2b5dd6a83ba9938c9b174b4d92f5d223
Thitirat12 <thitirat.phua@bumail.net>
2026-04-12 22:05:59 +0700

    PlayerController Add
    - Walk/Jump

--- Assets/Test/LV02_W/Script/Player/PlayerController.cs
+++ Assets/Test/LV02_W/Script/Player/PlayerController.cs
@@ -2,15 +2,99 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement")]
+    public float moveSpeed;
+    private float moveInput;
+
+    [Header("Jump")]
+    public float jumpForce;
+    private bool isGrounded;
+
+    [Header("Grounded")]
+    public Transform groundCheck;
+    public Vector2 checkRadius;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        HandleInput();
+        CheckGround();
+        HandleJump();
+        Flip();
     }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleInput()
+    {
+        moveInput = Input.GetAxis("Horizontal");    
+    }
+
+    void HandleMovement()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed,rb.linearVelocity.y);
+    }
+
+    void HandleJump()
+    {
+        if (!Input.GetButtonDown("Jump")) return;
+
+        if (Input.GetButtonDown("Jump") && isGrounded && rb.linearVelocity.y <= 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+
+            Debug.Log("Jump");
+        }
+
+        else
+        {
+            Debug.Log("Already Jump");
+        }
+    }
+
+    void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, checkRadius, 0f,groundLayer);
+    }
+
+    void Flip()
+    {
+        if (moveInput > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveInput < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+    }
+
+    void FlipCharacter()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(groundCheck.position, checkRadius);
+    }
+
 }
