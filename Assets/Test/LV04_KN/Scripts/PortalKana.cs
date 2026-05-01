185478e674a658d9b1b462170e41718b9d77523e
Thitirat12 <thitirat.phua@bumail.net>
2026-05-02 02:21:47 +0700

    Add Portal

---
+++ Assets/Test/LV04_KN/Scripts/PortalKana.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalKana : MonoBehaviour
+{
+    public string sceneName;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Next level");
+            SceneManager.LoadScene(sceneName);
+        }
+
+        else
+        {
+            return;
+        }
+    }
+}
